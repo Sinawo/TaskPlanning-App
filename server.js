a0a0c4f90d88734c55126175b958d2098d0a537a
Sinawo <sinawomngxuma@gmail.com>
2024-11-13 23:24:04 +0200

    Welcome page styles & image done

--- server.js
+++ server.js
@@ -8,16 +8,21 @@ const PORT = process.env.PORT || 3000;
 
 
 //this is to allow the application o eccepts json when seding json format file
-app.use(express.json()); // Middleware to parse JSON data
+
+app.use(express.json());// Middleware to parse JSON data
 app.use(cors()); // This will allow cross-origin requests from any origin
+
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Import routes || I  am importing the from routes (The fuctions point to controllers)
 const userRoutes = require('./routes/users');
 const workspaceRoutes = require('./routes/workspaces');
 const taskRoutes = require('./routes/tasks');
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Use routes
@@ -28,7 +33,7 @@ app.use('/tasks', taskRoutes);
 
 // Route to serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'welcome.html'));
 });
 
 app.listen(PORT, () => {
